07424805e8ebd579bedc45ef7ed3821c86c4bf5d
iampro3 <lucky1977@daum.net>
2023-04-19 17:37:42 +0900

    0419_if문 test

---
+++ basic.py
@@ -0,0 +1,42 @@
+#-*- utf-8 -*-
+
+
+""" if score >= 60:
+  message = "success"
+  else:
+    message = "failure" """
+
+""" prompt = """  """
+number = 0
+while number != 4:
+    print(prompt)
+    number = int(input()) """
+
+""" coffee = 10
+
+while True:
+  money = int(input("돈을 넣어주세요:"))
+  if money == 300:
+    print("커피를 줍니다.")
+    coffee = coffee -1
+  elif money >300:
+    print("거스름돈 %d를 주고 커피를 줍니다." % (money -300))
+    print("남은 커피의 양은 %d입니다." % coffee)
+    coffee = coffee-1
+  else:
+    print("돈을 다시주고 커피를 주지 않습니다.")
+    print("남은 커피의 양은 %d입니다." % coffee)
+
+  if coffee == 0:
+    print("커피가 다 떨어졌습니다. 중지합니다.")
+    break """
+
+""" 
+----- while
+a=0
+while 1<10:
+    a=a+1
+    if a % 2 == 0:continue
+    print(a)
+    break """
+
